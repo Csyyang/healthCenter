50e513a8ff2802797ab34af07156dbefb7d858ef
csy23333 <693765678@qq.com>
2022-06-28 16:47:29 +0800

    健康首页

--- src/pages/health/index.vue
+++ src/pages/health/index.vue
@@ -7,7 +7,23 @@
       <VBar id="bar2" key="bar2" color="#FFAA01" :text="['热量','千卡']" grey="#F2EDE3" />
     </div>
 
-    <VCardBox>
+    <VCardBox key="first">
+      23322
+    </VCardBox>
+
+    <VCardBox title="体重管理" date="06/23" name="体重：" value="90.0公斤" key="second">
+      23322
+    </VCardBox>
+
+    <VCardBox title="血压" date="06/23" name="正常高值：" value="113/92 mmhg" key="second">
+      23322
+    </VCardBox>
+
+    <VCardBox title="静息心率" date="06/23" name="平均心率：" value="80次/分钟" key="heart">
+      23322
+    </VCardBox>
+
+    <VCardBox title="身高" date="06/23" name="身高：" value="172cm" key="heart">
       23322
     </VCardBox>
 
