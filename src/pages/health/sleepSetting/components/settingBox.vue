23d70cd67d836624ebf431b14212db79dc8ae605
csy23333 <693765678@qq.com>
2022-06-29 17:59:44 +0800

    Create settingBox.vue

---
+++ src/pages/health/sleepSetting/components/settingBox.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="setting-box">
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.setting-box {
+  margin-top: 24px;
+}
+</style>
