a1a10f1f06911ec4aa34e2da90c0a132c44380ca
csy23333 <693765678@qq.com>
2022-06-30 09:51:49 +0800

    暂存

--- src/pages/health/sleepSetting/components/settingBox.vue
+++ src/pages/health/sleepSetting/components/settingBox.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="setting-box">
-
+    <div class="title-left">
+      <slot name="img">
+        <img :src="require('@img/moon.png')">
+      </slot>
+      {{ name }}
+    </div>
+    <div class="icon">
+      <img :src="require('@img/label')">
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    name: {
+      type: String,
+      default: "早睡",
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
